857ba3b8c1eca2d908933231c3306af57cd33a19
Aleksandr_Duma <Dumaweb9@gmail.com>
2022-06-16 23:47:15 +0300

    Add timer on block promo

--- app/js/main.js
+++ app/js/main.js
@@ -38,6 +38,70 @@ document.querySelectorAll('.select-wrap').forEach(function(selectWrap){
 	});
 });
 
+//........................timer.........................
+
+document.addEventListener('DOMContentLoaded', function() {
+	const lastDate = new Date('September 18 2022 00:00:00');
+
+	const daysVal = document.querySelector('.value-day');
+	const hoursVal = document.querySelector('.value-hours');
+	const minutesVal = document.querySelector('.value-minutes');
+	const secondsVal = document.querySelector('.value-seconds');
+
+	const daysText = document.querySelector('.text-day');
+	const hoursText = document.querySelector('.text-hours');
+	const minutesText = document.querySelector('.text-minutes');
+	const secondsText = document.querySelector('.text-seconds');
+
+	// функция склонения числительных
+	function declOfNum(number, titles) {  
+	    let cases = [2, 0, 1, 1, 1, 2];  
+	    return titles[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];  
+	}
+
+	const timeCount = function() {
+		let now = new Date();
+		let leftUntil = lastDate - now;
+		let days = Math.floor(leftUntil / 1000 / 60 / 60 / 24);
+		let hours = Math.floor(leftUntil / 1000 / 60 / 60) % 24;
+		let minutes = Math.floor(leftUntil / 1000 / 60) % 60;
+		let seconds = Math.floor(leftUntil / 1000) % 60;
+
+		if (leftUntil < 0) {
+			document.querySelector('.timer__content').textContent = 'Акция завершена!';
+		}
+
+		daysVal.textContent = days;
+		if (days < 10) {
+			daysVal.textContent = `0${days}`;
+		}
+
+		hoursVal.textContent = hours;
+		if (hours < 10) {
+			hoursVal.textContent = `0${hours}`;
+		}
+
+		minutesVal.textContent = minutes;
+		if (minutes < 10) {
+			minutesVal.textContent = `0${minutes}`;
+		}
+
+		secondsVal.textContent = seconds;
+		if (seconds < 10) {
+			secondsVal.textContent = `0${seconds}`;
+		}
+		// функция склонения: по необходимости меняем на русский текст
+		daysText.textContent = declOfNum(days, ['Day', 'Days', 'Days']);	
+		hoursText.textContent = declOfNum(hours, ['Hour', 'Hours', 'Hours']);
+		minutesText.textContent = declOfNum(minutes, ['Min', 'Mins', 'Mins']);
+		secondsText.textContent = declOfNum(seconds, ['Sec', 'Sec', 'Sec']);
+	}
+
+	timeCount();
+	setInterval(timeCount, 1000);
+
+});
+
 /*-----------------------header-sale-----------------------*/
 
 const headerSale = document.querySelector('.header__sale');
